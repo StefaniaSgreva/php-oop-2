45380689f46a1b6a5544a4c71e93556bbb1f4db2
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-12-14 15:53:08 +0100

    improved categories icons

--- index.php
+++ index.php
@@ -12,22 +12,22 @@ require_once __DIR__ . '/Models/Kennel.php';
 //PRODOTTI
 
 $foods = [
-    new Food('https://b2b.puntorigenera.com/82901-thickbox_default/cartuccia-hp-302xl-nera-compatibile-per-hp-3830-3832-4650-1110-2130-3630-4520-f6u68ae-capacita-15ml-480-pagine.jpg', 'Pollo', 'Monge', 28.60, 20, new Category("Cane"), '3kg', 'adult'),
-    new Food('https://www.robinsonpetshop.it/12918-thickbox_default/prolife-sterilised-sensitive-adult-quaglia-e-patate-per-gatti.jpg', 'Quaglia e Patate', 'prolife', 52.30, 10, new Category("Gatto"), '8kg', 'adult'),
-    new Food('https://www.petschoice.it/download/61ce67d_1stchoicecucciolorosa.jpg', 'Agnello, Pesce e Riso', 'Puppy Chiot', 70.20, '', new Category("Cane"), '15kg', 'puppy'),
-    new Food('https://www.croccantinibio.it/wp-content/uploads/2020/03/crocchette-ultima-cane.jpg', 'Bovino e Cereali', 'ultima', 35.80, '25', new Category("Cane"), '3kg', 'adult'),
+    new Food('https://b2b.puntorigenera.com/82901-thickbox_default/cartuccia-hp-302xl-nera-compatibile-per-hp-3830-3832-4650-1110-2130-3630-4520-f6u68ae-capacita-15ml-480-pagine.jpg', 'Pollo', 'Monge', 28.60, 20, new Category('<i class="fa-solid fa-shield-dog"></i>' . '<span class="categoryNameDog">Cane</span>'), '3kg', 'adult'),
+    new Food('https://www.robinsonpetshop.it/12918-thickbox_default/prolife-sterilised-sensitive-adult-quaglia-e-patate-per-gatti.jpg', 'Quaglia e Patate', 'prolife', 52.30, 10, new Category('<i class="fa-solid fa-shield-cat"></i>' . '<span class="categoryNameCat">Gatto</span>'), '8kg', 'adult'),
+    new Food('https://www.petschoice.it/download/61ce67d_1stchoicecucciolorosa.jpg', 'Agnello, Pesce e Riso', 'Puppy Chiot', 70.20, '', new Category('<i class="fa-solid fa-shield-dog"></i>' . '<span class="categoryNameDog">Cane</span>'), '15kg', 'puppy'),
+    new Food('https://www.croccantinibio.it/wp-content/uploads/2020/03/crocchette-ultima-cane.jpg', 'Bovino e Cereali', 'ultima', 35.80, '25', new Category('<i class="fa-solid fa-shield-dog"></i>' . '<span class="categoryNameDog">Cane</span>'), '3kg', 'adult'),
 ];
 $games = [
-    new Game('https://i5.walmartimages.com/asr/a915e9cc-5517-4e4c-aa7f-6286b0efd5e7_1.d5a864a08084249b9f7a43857cf6527b.jpeg?odnWidth=1000&odnHeight=1000&odnBg=ffffff', 'Palla', 'Felix', 5.50, '', new Category("Cane"), 'blu'),
-    new Game('https://www.loradeglianimali.it/35298-thickbox_default/trixie-dog-disc-frisbee-di-plastica-per-cani-da-19-cm.jpg', 'Frisbee', 'Sky-dogs', 10.60, 5, new Category("Cane"), 'blu'),
-    new Game('https://shop.risparmiocasa.com/media/catalog/product/cache/627b2652e996e22070c66f898fac62f5/1/3/1386848_1.jpg', 'Punch Ball', 'Petline', 12.30, '', new Category("Gatto"), 'blu'),
-    new Game('https://molinopisoni.it/50903-large_default/karlie-gioco-per-gatti-topo-loco.jpg', 'Topolini', 'Karlie', 15.60, '10', new Category("Gatto"), 'colori assortiti'),
+    new Game('https://i5.walmartimages.com/asr/a915e9cc-5517-4e4c-aa7f-6286b0efd5e7_1.d5a864a08084249b9f7a43857cf6527b.jpeg?odnWidth=1000&odnHeight=1000&odnBg=ffffff', 'Palla', 'Felix', 5.50, '', new Category('<i class="fa-solid fa-shield-dog"></i>' . '<span class="categoryNameDog">Cane</span>'), 'blu'),
+    new Game('https://www.loradeglianimali.it/35298-thickbox_default/trixie-dog-disc-frisbee-di-plastica-per-cani-da-19-cm.jpg', 'Frisbee', 'Sky-dogs', 10.60, 5, new Category('<i class="fa-solid fa-shield-dog"></i>' . '<span class="categoryNameDog">Cane</span>'), 'blu'),
+    new Game('https://shop.risparmiocasa.com/media/catalog/product/cache/627b2652e996e22070c66f898fac62f5/1/3/1386848_1.jpg', 'Punch Ball', 'Petline', 12.30, '', new Category('<i class="fa-solid fa-shield-cat"></i>' . '<span class="categoryNameCat">Gatto</span>'), 'blu'),
+    new Game('https://molinopisoni.it/50903-large_default/karlie-gioco-per-gatti-topo-loco.jpg', 'Topolini', 'Karlie', 15.60, '10', new Category('<i class="fa-solid fa-shield-cat"></i>' . '<span class="categoryNameCat">Gatto</span>'), 'colori assortiti'),
 ];
 $kennels = [
-    new Kennel('https://www.cucciatiragraffi.it/wp-content/uploads/2020/02/cuccia-gatto-sacco.jpg', 'Cuccia sacco', 'Felix', 25.50, '', new Category("Gatto"), 'Smoll'),
-    new Kennel('https://www.cucciapercani.info/wp-content/uploads/2018/08/pets-imperial-sussex-casa-per-cani-di-taglia-media-in-legno-1.jpg', 'Casetta in legno', 'Pets Imperial', 68.90, 10, new Category("Cane"), 'Large'),
-    new Kennel('https://www.in-vendita.it/sh-img/habicat-cuccia-occhi-di-gatto-01-600x800_cuccia%2Bgatti.jpg', 'Cuccia in legno', 'Felix', 35.20, '', new Category("Gatto"), 'Medium'),
-    new Kennel('https://www.miciogatto.it/new/wp-content/uploads/2017/12/cuccia-per-gatti-leone-686x705.jpg', 'Cuccia Leone', 'Royal Cat', 45.50, '', new Category("Gatto"), 'Smoll'),
+    new Kennel('https://www.cucciatiragraffi.it/wp-content/uploads/2020/02/cuccia-gatto-sacco.jpg', 'Cuccia sacco', 'Felix', 25.50, '', new Category('<i class="fa-solid fa-shield-cat"></i>' . '<span class="categoryNameCat">Gatto</span>'), 'Smoll'),
+    new Kennel('https://www.cucciapercani.info/wp-content/uploads/2018/08/pets-imperial-sussex-casa-per-cani-di-taglia-media-in-legno-1.jpg', 'Casetta in legno', 'Pets Imperial', 68.90, 10, new Category('<i class="fa-solid fa-shield-dog"></i>' . '<span class="categoryNameDog">Cane</span>'), 'Large'),
+    new Kennel('https://www.in-vendita.it/sh-img/habicat-cuccia-occhi-di-gatto-01-600x800_cuccia%2Bgatti.jpg', 'Cuccia in legno', 'Felix', 35.20, '', new Category('<i class="fa-solid fa-shield-cat"></i>' . '<span class="categoryNameCat">Gatto</span>'), 'Medium'),
+    new Kennel('https://www.miciogatto.it/new/wp-content/uploads/2017/12/cuccia-per-gatti-leone-686x705.jpg', 'Cuccia Leone', 'Royal Cat', 45.50, '', new Category('<i class="fa-solid fa-shield-cat"></i>' . '<span class="categoryNameCat">Gatto</span>'), 'Smoll'),
 ];
 // var_dump($foods, $games, $kennels);
 
